e90488208370cebc3a315b17cd211ace0525d568
techelevatortest <kitchenmaker@chef.net>
2022-09-17 19:34:31 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { UserComponent } from './components/user/user.component';
+import { HomeComponent } from './structure/home/home.component';
+import { HeaderComponent } from './structure/header/header.component';
+import { FooterComponent } from './structure/footer/footer.component';
+import { DashboardRoutingModule } from './features/dashboard/dashboard-routing.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		SigninComponent,
+		SignupComponent,
+		DashboardComponent,
+		UserComponent,
+		HomeComponent,
+		HeaderComponent,
+		FooterComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		DashboardRoutingModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+
+	database: {
+		baseUrl: "https://www.mypersonalkitchen.com/globals/database/",
+		select: "select.php",
+		select2: "select2.php",
+		selectIn: "selectIn.php",
+		selectLike: "selectLike.php",
+		selectLikeTwoColumns: "selectLikeTwoColumns.php",
+		insert: "insert.php",
+		update: "update.php",
+		delete: "delete.php",
+		delete2: "delete2.php",
+		deleteIn: "deleteIn.php",
+		fileMover: "fileMover.php",
+		photos: "photos.php",
+		fileImageMover: "fileImageMover"
+	}
 };
 
 /*
